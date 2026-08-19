bba3deb7f09ee944ef5f41f24e32e5f72cf4d1bf
Ian-Woo Kim <iank@mercury.com>
2026-08-19 14:25:33 -0700

    Fix broken tests.
    Change-Id: I25c72b238ee9a5928ed641052dd786a5ae8e8625

--- tests/build_tests/rule_configs/Lib.hs
+++ tests/build_tests/rule_configs/Lib.hs
@@ -1,8 +1,11 @@
 -- | Tests that we can use haddock when the exports depend on toolchain
 -- libraries
-module Lib(dependOnExample, Example) where
+module Lib(greeting, dependOnExample, Example) where
 
 import Test.Hspec (Example)
+
+greeting :: String
+greeting = "Hello from Lib"
 
 -- | We refer to 'Example' to test haddock.
 dependOnExample :: Example () => ()
